94efb9fbd1a1fe237e23fac5bfa019ce23a98079
Mário Igor de Jesus <167040097+marioigor1982@users.noreply.github.com>
2026-09-01 22:02:15 -0300

    fix: update broccoli tapioca image source
    Update the image path for the broccoli tapioca to the local assets directory.

--- constants.ts
+++ constants.ts
@@ -146,7 +146,7 @@ export const SALTY_TAPIOCAS: MenuItem[] = [
     id: 34,
     name: 'BRÓCOLIS',
     description: 'QUEIJO MINAS RALADO, BRÓCOLIS, REQUEIJÃO E ORÉGANO.',
-    images: [{ url: 'https://i.postimg.cc/6QYHChWm/BR-COLIS.jpg', isMain: true }],
+    images: [{ url: '/images/tapiocas/salgadas/tapioca-de-brocolis.jpg', isMain: true }],
     price: 'R$ 13,00',
   },
   {
